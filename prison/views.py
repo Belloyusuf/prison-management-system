b68f88953f48903f3db65a83dc19ffa5962de94a
bello yusuf <belloyusuf1997@gmail.com>
2023-11-19 19:19:12 +0100

    Working on views, first commit.

--- prison/views.py
+++ prison/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Prisoner, Onbail, Oncourt
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 
-# Create your views here.
+
+
+
+
+
+class PrisonerCreateView(CreateView):
+    model = Prisoner
+    fields = "__all__"
+    template_name = "prisoner/create.html"
+    success_url = "list"
+    
+
+
+
+
+
+class PrisonerListView(ListView):
+    model = Prisoner
+    template_name = "prisoner/list.html"
+    context_object_name = "prisoners"
+
+
+
