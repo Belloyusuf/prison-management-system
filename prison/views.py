5c1b1549590fdd230ccac8392e8ac27ff3519873
bello yusuf <belloyusuf1997@gmail.com>
2023-11-19 21:32:14 +0100

    Modify the views, I changed it from class based views to function based views.

--- prison/views.py
+++ prison/views.py
@@ -1,27 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, request
+from django.http import Http404
 from .models import Prisoner, Onbail, Oncourt
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+# from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 
 
 
+def prisonerList(request):
+    prisoner = Prisoner.objects.all()
+    return render(request, "prisoner/list.html", {
+        'prisoner':prisoner
+    })
 
 
 
-class PrisonerCreateView(CreateView):
-    model = Prisoner
-    fields = "__all__"
-    template_name = "prisoner/create.html"
-    success_url = "list"
-    
-
-
-
-
-
-class PrisonerListView(ListView):
-    model = Prisoner
-    template_name = "prisoner/list.html"
-    context_object_name = "prisoners"
-
-
+def prisonerDetail(request, prisoner_id):
+    try:
+        prisoner = Prisoner.objects.get(pk=prisoner_id)
+    except Prisoner.DoesNotExist:
+        raise Http404("Prisoner doesn't exist")
+    return render(request, "prisoner/detail.html", {
+        'prisoner':prisoner
+    })
 
